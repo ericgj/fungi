d014ee7c996ad5c307141744677e0d442e55fd52
Eric Gjertsen <ericgj72@gmail.com>
2016-06-13 13:24:01 -0400

    draft Union type match function
      - note: assumes union of iterable types (namedtuples, e.g.)
    
    add typing lib dependency

---
+++ src/unionutil.py
@@ -0,0 +1,33 @@
+
+
+def match(uniontype,cases,target):
+  assert issubclass(target.__class__,uniontype), \
+    "%s is not in union type" % target.__class__.__name__
+
+  missing = [
+    t.__name__ for t in uniontype.__union_set_params__ 
+      if not (cases.has_key(type(None)) or cases.has_key(t))
+  ]
+  assert len(missing) == 0, \
+    "No case found for the following type(s): %s" % ", ".join(missing)
+
+  fn = None
+  wildcard = False
+  try:
+    fn = ( 
+      next( cases[klass] for klass in cases if isinstance(target,klass) )
+    )
+
+  except StopIteration:
+    fn = cases.get(type(None),None)
+    wildcard = bool(fn)
+
+  if fn is None:
+    raise TypeError("No cases match %s" % target.__class__.__name__)
+  
+  assert callable(fn), \
+    "Matched case is not callable; check your cases"
+
+  return fn() if wildcard else fn( *(slot for slot in target) )
+
+
