1c71d2f9b9d746ac495515393b98c610a39f0f4d
Eric Gjertsen <ericgj72@gmail.com>
2016-06-27 17:45:05 -0400

    gae.user.login_url returns Either, not Task

--- fungi/gae/user.py
+++ fungi/gae/user.py
@@ -2,12 +2,15 @@ from google.appengine.api import users
 
 from pymonad.Maybe import Just, Nothing
 from pymonad_extra.util.maybe import with_default
+from pymonad.Either import Left, Right
+from pymonad_extra.util.either import fold
 from pymonad_extra.Task import Task
 from pymonad_extra.util.task import reject, resolve
 import fungi.util.err as err
 from fungi.wsgi import redirect_response
 
 def current():
+  # () -> Maybe User
   try:
     u = users.get_current_user()
     if u is None:
@@ -19,20 +22,19 @@ def current():
 
 
 def login_url(dest_url):
-  def _login_url(rej,res):
-    try:
-      res( users.create_login_url(dest_url=dest_url) )
-    except Exception as e:
-      rej( err.wrap(e) )
-  return Task(_login_url)
+  # String -> Either Exception String
+  try:
+    return Right( users.create_login_url(dest_url=dest_url) )
+  except Exception as e:
+    return Left( err.wrap(e) )
 
 
 def current_or_redirect_to_login(dest_url):
-  # String -> Task HTTPRedirection User
+  # String -> Task (HTTPRedirection | Exception) User
 
   muser = current()
   return with_default(
-    (login_url(dest_url).fmap(redirect_response)) >> reject,
+    fold(reject, reject, login_url(dest_url).fmap(redirect_response)),
     muser.fmap(resolve)
   )
 
