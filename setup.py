7f2ea8d8c08f73deaa1623f940ff8a07c3160304
Eric Gjertsen <ericgj72@gmail.com>
2016-08-15 19:06:05 -0400

    version bump (0.8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='fungi',
-  version='0.7',
+  version='0.8',
   description='Functional Gateway Interface Drug',
   url='https://github.com/ericgj/fungi',
   author='Eric Gjertsen',
